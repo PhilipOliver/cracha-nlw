d06a316dfe21de6644f62b08df11e8ae1678c2c2
Philip Oliver <69124915+PhilipOliver@users.noreply.github.com>
2021-10-23 10:11:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// https://api.github.com/
+// links das redes sociais atribuidas aos icones
+const LinksSocialMedia = {
+  github: "PhilipOliver",
+  youtube: "UCR4O3XW6JEPR4wJlSsY7lNw",
+  facebook: "philipinho.oliver",
+  instagram: "philipoliver726",
+  twitter: "PhilipOliv3r"
+}
+
+// substituindo os links
+function changeSocialMediaLinks() {
+  for(let li of socialLinks.children){
+    const social = li.getAttribute('class')
+    
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  // Pegando o JSON
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  // Transformando em JSON
+  fetch(url)
+    .then(response => response.json() )
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
